b7b55972c42ef792a7625b0c2effd1f381d7831c
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2018-11-19 20:52:40 +0530

    Form

--- src/components/login.js
+++ src/components/login.js
@@ -10,7 +10,14 @@ class LoginPage extends React.Component {
 		super(props);
 		this.state = {
 			activeTab : '1',
-
+			l01 : "",
+			l02 : "",
+			r01 : "",
+			r02 : "",
+			r03 : "",
+			r04 : "",
+			loginBtn : false,
+			registerBtn : false
 		};
 		this.toggle = (tab) => {
 			if(this.state.activeTab !== tab) {
@@ -19,11 +26,33 @@ class LoginPage extends React.Component {
 				});
 			}
 		}
+		this.onChange = (id, value) => {
+			this.setState({
+				[id] : value
+			});
+			if(this.state.l01 !== "" && this.state.l02 !== "") {
+				this.setState({
+					loginBtn : true
+				});
+			}
+			if(this.state.r01 !== "" && this.state.r02 !== "" && this.state.r03 !== "" ) {
+				this.setState({
+					registerBtn : true
+				});
+			}
+		}
 
 	}
 	render() {
+		var lProp = {};
+		if(!this.state.loginBtn) {
+			lProp['disabled'] = 'disabled';
+		}
+		var rProp = {};
+		if(!this.state.registerBtn) {
+			rProp['disabled'] = 'disabled';
+		}
 		return (
-				
 				<div>
 					<br/><br/><br/><br/>
 					<Container>
@@ -50,17 +79,18 @@ class LoginPage extends React.Component {
 								<FormGroup row>
 									<Label sm = {2}>E-Mail</Label>
 									<Col sm = {10}>
-										<Input type = "email" name = "email" placeholder = "Enter Email" />
+										<Input type = "email" name = "email" placeholder = "Enter Email" onChange = {e => {this.onChange("l01",e.target.value)}}/>
 									</Col>
+									{console.log(this.state.l01)}
 								</FormGroup>
 								<FormGroup row>
 									<Label sm = {2}>Password</Label>
 									<Col sm = {10}>
-										<Input type = "password" name = "password" placeholder = "Password" />
+										<Input type = "password" name = "password" placeholder = "Password" onChange = {e => {this.onChange("l02",e.target.value)}}/>
 									</Col>
 								</FormGroup>
 								</Form>
-								<Button color = "warning">Submit</Button>
+								<Button color = "warning" {...lProp}>Submit</Button>
 								<br/><br/>
 								<a> Or </a>
 								<img src = {googleSignin} width = "30%" height = "30%"   />
@@ -74,29 +104,29 @@ class LoginPage extends React.Component {
 										<FormGroup row>
 											<Label sm = {2}>Name</Label>
 											<Col sm = {10}>
-												<Input type = "text" name = "Name" placeholder = "Name " />
+												<Input type = "text" name = "Name" placeholder = "Name " onChange = {e => {this.onChange("r01",e.target.value)}}/>
 											</Col>
 										</FormGroup>
 										<FormGroup row>
 											<Label sm = {2}>E-Mail</Label>
 											<Col sm = {10}>
-												<Input type = "email" name = "email" placeholder = "E-Mail " />
+												<Input type = "email" name = "email" placeholder = "E-Mail " onChange = {e => {this.onChange("r02",e.target.value)}}/>
 											</Col>
 										</FormGroup>
 										<FormGroup row>
 											<Label sm = {2}>Password</Label>
 											<Col sm = {10}>
-												<Input type = "password" name = "password1" placeholder = "Password " />
+												<Input type = "password" name = "password1" placeholder = "Password " onChange = {e => {this.onChange("r03",e.target.value)}}/>
 											</Col>
 										</FormGroup>
 										<FormGroup row>
 											<Col sm = {2}></Col>
 											<Col sm = {10}>
-												<Input type = "password" name = "password2" placeholder = "Re-Enter Password 	" />
+												<Input type = "password" name = "password2" placeholder = "Re-Enter Password " onChange = {e => {this.onChange("r04",e.target.value)}}/>
 											</Col>
 										</FormGroup>
 									</Form>	
-									<Button color = "warning" >Submit</Button>
+									<Button color = "warning" {...rProp}>Submit</Button>
 									<br/><br/>
 								</Container>
 							</TabPane>
